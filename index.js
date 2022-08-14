dfa20c649ff58a22270c1e868c6dbd4a48314ed3
Yuchen Chen <angelchen3311@gmail.com>
2022-08-14 18:59:00 -0400

    commit tree 67abceb3eadc499a2c8b5eef2f77189fac454bda

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// function totalBatteries(batteries) {
+//     let totalBatteries = 31;
+//     for (const batteryBatches of batteries) {
+//         totalBatteries += batteryBatches;
+//     };
+//     return totalBatteries
+// }
+
+const totalBatteries = 0;
+const reducer = (accumulator, item) => {
+    return (accumulator += item);
+};
+const total = batteryBatches.reduce(reducer , totalBatteries);
+
+
+
+
+// const total = batteryBatches.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue;
+// }, 0);
